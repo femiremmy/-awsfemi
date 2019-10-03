81ef844912b3e661a22efb5f59db6e66055a1164
root <root@localhost.localdomain>
2019-10-03 12:22:52 -0400

    new file for ec2

---
+++ ec2new/ec2file.tf
@@ -0,0 +1,13 @@
+reate a new instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "HelloWorld"
+provider "aws" {
+  region = "us-west-1"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-09c61c4850b7465cb"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "First Trerraform EC2"
+  }
+}
